6872c7927f2fbc2170fa6e0981457030908926a6
JhingaMan <kunalgupta1208@gmail.com>
2024-11-07 13:00:11 +0530

    feat: Registration of user backend

--- Bf-remover-server/server.js
+++ Bf-remover-server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectdb from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -15,5 +16,7 @@ await connectdb()
 // API routes
 
 app.get("/", (req, res) => res.send("API Working"));
+
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
